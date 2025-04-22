24d6b30a90f1a95491949b7ad8baf48d899f15b8
Mark Watson <mlw335@cbsulm35.biohpc.cornell.edu>
2025-04-22 17:51:26 -0400

    late update, uploading all analyses and associated figures (bad links in Rmd Files)

---
+++ code/colors.R
@@ -0,0 +1,42 @@
+# Setting colors for commmunity 
+community_colors <- c(
+  "empty" = "#1d4ea4",
+  "native" = "#0081b8",
+  "depleted" = "#00a482",
+  "invaded" = "#93b840")
+
+
+# Setting colors for treatment 
+treatment_colors <- c(
+  "primary_treatment" = "#0e7840",
+  "secondary_treatment" = "#0081b8",
+  "raw_water" = "#b8b32e")
+
+  
+
+# Set the phylum colors
+phylum_colors <- c(
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Bacteroidota = "gold", 
+  Betaproteobacteria = "plum1", 
+  Bdellovibrionota = "red1",
+  Chloroflexi="black", 
+  "Candidatus Kapabacteria" = "firebrick",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  "Marinimicrobia (SAR406 clade)" = "yellow",
+  Myxococcota = "#B5D6AA",
+  Pseudomonadota = "royalblue",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Proteobacteria_unclassified = "greenyellow",
+  Thermoplasmatota = "green",
+  Thermoproteota = "palevioletred1",
+  Verrucomicrobiota = "darkorchid1",
+  Other = "grey")
